f2346b33c72c3c39fcc55782e85f1edd42cff330
Mark A <mark001616@gmail.com>
2023-07-26 20:56:22 -0400

    Add solution & tests for problem 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,15 @@
-const repeatString = function() {
+const repeatString = function (text, amountToRepeat) {
+    let output = ''
+    if (amountToRepeat < 0) {
+      return "ERROR";
+    } else {
+      for (let x = 0; x < amountToRepeat; x++) {
+        output += text;
+      }
+      return output;
+    }
+    
+    
 
 };
 
