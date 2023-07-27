5742b3cbc8643055e9aef9e348620d3e64c7b75d
Mark A <mark001616@gmail.com>
2023-07-26 21:24:48 -0400

    Add solution and tests for problem 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function (text) {
+  let reversedText = "";
+  for (let x = text.length - 1; x >= 0; x--) {
+    reversedText += text[x];
+  }
+  return reversedText;
 };
 
 // Do not edit below this line
