1466543a7974de6e2a47eb9dcd7ac77215940eb1
Mark A <mark001616@gmail.com>
2023-07-27 19:28:01 -0400

    Add solution and tests for problem 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function (first, second) {
+  let start = first;
+  let end = second;
 
+  if (first > second) {
+    // Swap start and end values if first is greater than second
+    start = second;
+    end = first;
+  }
+  if (first < 0 || second < 0) {
+    return "ERROR";
+  }
+  if (typeof first != "number" || typeof second != "number") {
+    return "ERROR";
+  }
+
+  let sum = 0;
+  for (let x = start; x <= end; x++) {
+    sum += x;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
