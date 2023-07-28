32764a5f19cd75d1bf318355c464f04644edffb1
Mark A <mark001616@gmail.com>
2023-07-27 20:46:03 -0400

    Add solutions & tests for problem 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheitTemperature) {
+  const FAHRENHEIT_TO_CELSIUS = ((fahrenheitTemperature - 32) * 5) / 9;
+  return parseFloat(FAHRENHEIT_TO_CELSIUS.toFixed(1)); // toFixed turns into string, parseFloat reverts to number/float 
 };
 
-const convertToFahrenheit = function() {
+
+
+const convertToFahrenheit = function (celsiusTemperature) {
+  const CELSIUS_TO_FAHRENHEIT = (celsiusTemperature / 5) * 9 + 32;
+  return parseFloat(CELSIUS_TO_FAHRENHEIT.toFixed(1)); // toFixed turns into string, parseFloat reverts to number/float
 };
 
 // Do not edit below this line
@@ -9,3 +15,26 @@ module.exports = {
   convertToCelsius,
   convertToFahrenheit
 };
+
+/*
+function "convertToCelsius":
+  Input: temperature in fahrenheit
+  Output:  temperature in celcius
+
+function "convertToFahrenheit":
+  input: temperature in celcius
+  output: temperature in fahrenheit
+
+note: Round up 100th decimal place.
+
+Steps to get the wanted output in convertToCelsius:
+  subtract 32 from the number, then multiply the result by 5. Finally divide that value by 9. 
+  round value by 100th decimal
+  Return the value, This is the temperature in celcius
+
+
+Steps to get the wanted output in convertToFahrenheit: 
+  Divide the celcius number by 5, then multiply the result by 9. Finally add 32 to the result. 
+  round value by 100th decimal
+  Return the value, this is the temperature in fahrenheit. 
+*/
