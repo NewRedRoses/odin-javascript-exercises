d9c924a92b15311ad4825fa924ae2122ef100801
Mark A <mark001616@gmail.com>
2023-08-10 03:43:21 -0400

    Add solution and test cases for problem 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,36 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let sum = 0;
+  sum = array.reduce((total, current) => {
+    return total + current;
+  }, 0);
+  return sum;
 };
 
-const multiply = function() {
-
+const multiply = function (values) {
+  let answer = 0;
+  answer = values.reduce((previous, currentValue) => {
+    return previous * currentValue;
+  }, 1);
+  return answer;
 };
 
-const power = function() {
-	
+const power = function (base, exponent) {
+  return base ** exponent;
 };
 
-const factorial = function() {
-	
+const factorial = function (number) {
+  if (number < 0) return "Undefined";
+  let fact = 1;
+  for (let i = number; i > 1; i--) fact *= i;
+  return fact;
 };
 
 // Do not edit below this line
@@ -29,5 +40,5 @@ module.exports = {
   sum,
   multiply,
   power,
-  factorial
+  factorial,
 };
