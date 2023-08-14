c61a1d696a289bb76d13ffaaacc250d7757e684e
Mark A <mark001616@gmail.com>
2023-08-14 17:04:25 -0400

    Add solutions & test solutions for 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (listOfPeople) {
+  let currentYear = new Date().getFullYear();
+  listOfPeople.reduce((previousPerson, currentPerson) => {
+    let age = currentPerson.yearOfDeath - currentPerson.yearOfBirth;
+    if (isNaN(age)) {
+        currentPerson.age = currentYear - currentPerson.yearOfBirth;
+    } else {
+      currentPerson.age = age;
+    }
+  }, 0);
+  listOfPeople.sort((personA, personB) => {
+    if (personA.age > personB.age) {
+      return -1;
+    } else return 1;
+  });
+  console.table(listOfPeople);
 
+  for (let person in listOfPeople) {
+    return listOfPeople[person];
+  }
 };
-
 // Do not edit below this line
 module.exports = findTheOldest;
