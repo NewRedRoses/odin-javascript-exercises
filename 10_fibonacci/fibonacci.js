bf4f0d98a65084e1b9a53739a6cb2a58ab88e5a6
Mark A <mark001616@gmail.com>
2023-08-14 15:09:23 -0400

    Add solutions & test solutions for 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function (number) {
+  if (number >= 0) {
+    let fibonacciNumbers = [];
+    let n1 = 0,
+      n2 = 1,
+      nextVal;
+    for (let x = 0; x <= number; x++) {
+      fibonacciNumbers.push(n1);
+      nextVal = n1 + n2;
+      n1 = n2;
+      n2 = nextVal;
+    }
+    return fibonacciNumbers[number];
+  } else {
+    return "OOPS";
+  }
 };
-
 // Do not edit below this line
 module.exports = fibonacci;
